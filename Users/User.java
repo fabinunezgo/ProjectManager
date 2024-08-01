edf38f7f304bb08589f3b1d47be525cc7af9ff8f
Fabiana <linunezgo@est.utn.ac.cr>
2024-08-01 15:34:06 -0600

    Clase users
    Se crea la clase users

---
+++ Users/User.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Users;
+
+/**
+ *
+ * @author fabia
+ */
+public class User {
+    private String id;
+    private String name;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+    }
+    
+}
--- projectmanager/lider/ProjectManagerLider.java
+++ projectmanager/lider/ProjectManagerLider.java
@@ -15,6 +15,7 @@ public class ProjectManagerLider {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
